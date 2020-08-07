c8510022ce4d700ce0d80d2b2860e5657707e4cb
NIKI <niki801226@hotmail.com>
2020-08-07 16:22:00 +0800

    calc avg-len

--- read.py
+++ read.py
@@ -1,5 +1,6 @@
 data = []
 count = 0
+length = 0 
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
@@ -8,7 +9,17 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0: # %求餘數
 			print(len(data))
 
+		length += len(line)
+
+avg_len = length / len(data)
+
 		
-print(len(data))
+print('檔案讀取完畢, 總共有', len(data), '筆資料')
+print('留言平均長度總共有', avg_len, '個字')
+
+
+
+print('第一筆留言:')
 print(data[0])
+
 
